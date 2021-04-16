e963080065c6614bd269be53f15629b08949040f
SrPierryON <81433543+SrPierryON@users.noreply.github.com>
2021-04-16 20:12:02 -0300

    Update remove.js

--- src/bot/commands/Bots/remove.js
+++ src/bot/commands/Bots/remove.js
@@ -5,12 +5,12 @@ const Bots = require("@models/bots");
 const { server: {mod_log_id, role_ids} } = require("@root/config.json");
 
 const reasons = {
-    "1": `Your bot was offline when we tried to verify it.`,
-    "2": `Your bot is a clone of another bot`,
-    "3": `Your bot responds to other bots`,
-    "4": `Your bot doesn't have any/enough working commands. (Minimum: 7)`,
-    "5": `Your bot has NSFW commands that work in non-NSFW marked channels`,
-    "6": `Your bot doesn't have a working help command or commands list`
+    "1": `Seu bot estava offline quando tentamos verificá-lo.`,
+    "2": `Seu bot é um clone de outro bot`,
+    "3": `Seu bot responde a outros bots`,
+    "4": `Seu bot não tem comandos de trabalho / suficientes. (Mínimo: 7)`,
+    "5": `Seu bot tem comandos NSFW que funcionam em canais não marcados com NSFW`,
+    "6": `Seu bot não tem um comando de ajuda ou lista de comandos funcionando`
 }
 var modLog;
 
@@ -22,17 +22,17 @@ module.exports = class extends Command {
             aliases: ["delete"],
             permissionLevel: 8,
             botPerms: ["SEND_MESSAGES"],
-            description: "Remove a bot from the botlist",
-            usage: '[Member:user]'
+            description: "Remova um bot da lista de bots",
+            usage: '[Membro:user]'
         });
     }
 
     async run(message, [Member]) {
-        if (!Member || !Member.bot) return message.channel.send(`You didn't ping a bot to remove.`)
+        if (!Member || !Member.bot) return message.channel.send(`Você não fez ping em um bot para remover.`)
         let e = new MessageEmbed()
-            .setTitle('Reasons')
+            .setTitle('Razões')
             .setColor(0x6b83aa)
-            .addField(`Removing bot`, `${Member}`)
+            .addField(`Removendo bot`, `${Member}`)
         let cont = ``;
         for (let k in reasons) {
             let r = reasons[k];
@@ -48,31 +48,31 @@ module.exports = class extends Command {
         let r = collected.first().content;
         if (parseInt(reason)) {
             r = reasons[reason]
-            if (!r) return message.channel.send("Inavlid reason number.")
+            if (!r) return message.channel.send("Número do motivo inválido.")
         }
 
         let bot = await Bots.findOne({ botid: Member.id }, { _id: false });
         await Bots.updateOne({ botid: Member.id }, { $set: { state: "deleted", owners: {primary: bot.owners.primary, additional: []} } });
         const botUser = await this.client.users.fetch(Member.id);
 
-        if (!bot) return message.channel.send(`Unknown Error. Bot not found.`)
+        if (!bot) return message.channel.send(`Erro desconhecido. Bot não encontrado.`)
         let owners = [bot.owners.primary].concat(bot.owners.additional)
         e = new MessageEmbed()
-            .setTitle('Bot Removed')
+            .setTitle('Bot removido')
             .addField(`Bot`, `<@${bot.botid}>`, true)
             .addField(`Owner`, owners.map(x => x ? `<@${x}>` : ""), true)
             .addField("Mod", message.author, true)
-            .addField("Reason", r)
+            .addField("Razão", r)
             .setThumbnail(botUser.displayAvatarURL({format: "png", size: 256}))
             .setTimestamp()
             .setColor(0xffaa00)
         modLog.send(e)
         modLog.send(owners.map(x => x ? `<@${x}>` : "")).then(m => { m.delete() });
-        message.channel.send(`Removed <@${bot.botid}> Check <#${mod_log_id}>.`)
+        message.channel.send(`Removido <@${bot.botid}> Verificar <#${mod_log_id}>.`)
         
         owners = await message.guild.members.fetch({user: owners})
         owners.forEach(o => {
-            o.send(`Your bot ${bot.username} has been removed:\n>>> ${r}`)
+            o.send(`Seu bot ${bot.username} foi removido:\n>>> ${r}`)
         })
         if (!message.client.users.cache.find(u => u.id === bot.botid).bot) return;
         try {
