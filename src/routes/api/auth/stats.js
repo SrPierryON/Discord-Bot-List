54a65f5b196b0140cb5d89066c7700998f02856f
SrPierryON <81433543+SrPierryON@users.noreply.github.com>
2021-04-16 17:11:53 -0300

    Update stats.js

--- src/routes/api/auth/stats.js
+++ src/routes/api/auth/stats.js
@@ -10,12 +10,12 @@ route.use(bodyParser.json({limit: '50mb'}));
 route.post('/:id', authApi, async (req, res) => {
     let count = req.body.count || req.body.server_count;
 
-    if (!count) return res.json({ success: "false", error: "Count not found in body." });
+    if (!count) return res.json({ success: "false", error: "Contagem não encontrada no corpo." });
     count = parseInt(count);
-    if (!count) return res.json({ success: "false", error: "Count not integer." });
+    if (!count) return res.json({ success: "false", error: "Contagem não inteira." });
 
     let bot = await Bots.findOne({ botid: req.params.id }, { _id: false })
-    if (bot.servers.length > 0 && bot.servers[bot.servers.length-1] &&  Date.now() - bot.servers[bot.servers.length-1].time < ratelimit * 1000) return res.json({ success: "false", error: "You are being ratelimited." });
+    if (bot.servers.length > 0 && bot.servers[bot.servers.length-1] &&  Date.now() - bot.servers[bot.servers.length-1].time < ratelimit * 1000) return res.json({ success: "false", error: "Você está sendo limitado pela taxa." });
     
     bot = await Bots.findOneAndUpdate({ botid: req.params.id }, {"$push":{"servers":{"$each": [{count}]}}}, { runValidators: true })
     res.json({ success: true });
