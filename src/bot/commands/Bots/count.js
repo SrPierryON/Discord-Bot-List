5fb177a1a1f94beb861ac5507ffa4486b75ee3de
SrPierryON <81433543+SrPierryON@users.noreply.github.com>
2021-04-16 20:04:32 -0300

    Update count.js

--- src/bot/commands/Bots/count.js
+++ src/bot/commands/Bots/count.js
@@ -10,14 +10,14 @@ module.exports = class extends Command {
             permLevel: 0,
             botPerms: ["SEND_MESSAGES"],
             requiredSettings: [],
-            description: "Check how many bots there are in the list."
+            description: "Verifique quantos bots existem na lista."
         });
     }
 
     async run(message) {
         let bots = await Bots.find({}, { _id: false })
         bots = bots.filter(bot => bot.state !== "deleted");
-        if (bots.length === 1) message.channel.send(`There is \`1\` bot in the list.`)
-        else message.channel.send(`There are \`${bots.length}\` bots in the list.`)
+        if (bots.length === 1) message.channel.send(`Há \`1\` bot na lista.`)
+        else message.channel.send(`Existem \`${bots.length}\` bots na lista.`)
     }
 };
