7ffcf7615fa1d88f8b5c681b9b3d47b151df3493
SrPierryON <81433543+SrPierryON@users.noreply.github.com>
2021-04-16 14:59:03 -0300

    Update search.js

--- src/routes/bots/search.js
+++ src/routes/bots/search.js
@@ -15,7 +15,7 @@ route.get("/", async (req, res) => {
         else if (bot.description.toLowerCase().includes(search)) return true;
         else return false;
     });
-    if (!found) return res.send({ error: "No bots found for this search" });
+    if (!found) return res.send({ error: "Nenhum bot encontrado para esta pesquisa" });
 
     res.render("search", {
         cards: found,
